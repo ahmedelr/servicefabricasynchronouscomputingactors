dac2983ec17a1b7166f4e94cd65379d508cb58e4
Ahmed El-Ramly <ahmedelr@users.noreply.github.com>
2017-05-19 01:50:55 -0400

    Add files via upload

---
+++ FilingService/FilingService.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Fabric;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.ServiceFabric.Services.Communication.Runtime;
+using Microsoft.ServiceFabric.Services.Runtime;
+using Microsoft.ServiceFabric.Services.Remoting;
+using Microsoft.ServiceFabric.Services.Remoting.Runtime;
+using Microsoft.Azure.Documents;
+using Microsoft.Azure.Documents.Client;
+using Microsoft.Azure.Documents.Linq;
+using System.Configuration;
+using Microsoft.AzureCat.Samples.Entities;
+
+
+/// This Service Fabric Service performs the initial Filing step of the CTMS process 
+/// by commiting the initial lab result message to DocumentDB
+namespace FilingService
+{
+    public interface IFileService: IService
+    {
+        Task<string> FiletoDBAsync(Q2Message message);
+    }
+    /// <summary>
+    /// An instance of this class is created for each service instance by the Service Fabric runtime.
+    /// </summary>
+    //internal sealed 
+    class FilingService : StatelessService, IFileService
+    {
+        // Setup DocumentDB variables
+        private static readonly string DatabaseId = ConfigurationManager.AppSettings["database"];
+        private static readonly string CollectionId = ConfigurationManager.AppSettings["collection"];
+        private static DocumentClient client;
+
+        public FilingService(StatelessServiceContext context)
+            : base(context)
+        { }
+
+        /// <summary>
+        /// Optional override to create listeners (e.g., TCP, HTTP) for this service replica to handle client or user requests.
+        /// </summary>
+        /// <returns>A collection of listeners.</returns>
+        protected override IEnumerable<ServiceInstanceListener> CreateServiceInstanceListeners()
+        {
+            //return new ServiceInstanceListener[0];
+            return new[] { new ServiceInstanceListener(context => this.CreateServiceRemotingListener(context)) };
+        }
+
+        // Create DB and Collection if necessary
+        public static void Initialize()
+        {
+            client = new DocumentClient(new Uri(ConfigurationManager.AppSettings["endpoint"]), ConfigurationManager.AppSettings["authKey"], new ConnectionPolicy { EnableEndpointDiscovery = false });
+            CreateDatabaseIfNotExistsAsync().Wait();
+            CreateCollectionIfNotExistsAsync().Wait();
+        }
+
+        // Create DB if it doesn't exist
+        private static async Task CreateDatabaseIfNotExistsAsync()
+        {
+            try
+            {
+                await client.ReadDatabaseAsync(UriFactory.CreateDatabaseUri(DatabaseId));
+            }
+            catch (DocumentClientException e)
+            {
+                if (e.StatusCode == System.Net.HttpStatusCode.NotFound)
+                {
+                    await client.CreateDatabaseAsync(new Database { Id = DatabaseId });
+                }
+                else
+                {
+                    throw;
+                }
+            }
+        }
+
+        // Create Collection if it doesn't exist
+        private static async Task CreateCollectionIfNotExistsAsync()
+        {
+            try
+            {
+                await client.ReadDocumentCollectionAsync(UriFactory.CreateDocumentCollectionUri(DatabaseId, CollectionId));
+            }
+            catch (DocumentClientException e)
+            {
+                if (e.StatusCode == System.Net.HttpStatusCode.NotFound)
+                {
+                    await client.CreateDocumentCollectionAsync(
+                        UriFactory.CreateDatabaseUri(DatabaseId),
+                        new DocumentCollection { Id = CollectionId },
+                        new RequestOptions { OfferThroughput = 1000 });
+                }
+                else
+                {
+                    throw;
+                }
+            }
+        }
+
+        // Create a DB record with the Json Lab Result payload
+        public async Task<string> FiletoDBAsync(Q2Message message)
+        {
+            await client.CreateDocumentAsync(UriFactory.CreateDocumentCollectionUri(DatabaseId, CollectionId), message);
+            return "Successfully inserted in DB";
+        }
+    }
+}
